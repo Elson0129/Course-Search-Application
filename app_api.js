9d4b573ccc9e0c8d9a34b58418a74aa20cf95fda
Elson Lin <elson@loopintegration.com>
2021-12-07 09:10:11 -0500

    Changed the port to resolve Heroku deployment issue

--- app_api.js
+++ app_api.js
@@ -2,7 +2,7 @@ var express = require('express');
 const router = require('./routes.js');
 var app = express();
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 var endpoints = require('./routes.js')
 app.use('/api', endpoints);
