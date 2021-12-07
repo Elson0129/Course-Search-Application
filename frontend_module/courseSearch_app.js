ddea482c74d9a7fb9bb046c56c9d899c4d6e0ddb
Elson Lin <elson@loopintegration.com>
2021-12-07 10:16:34 -0500

    Changed api endpoints to resolve Heroku deployment error

--- frontend_module/courseSearch_app.js
+++ frontend_module/courseSearch_app.js
@@ -1,7 +1,5 @@
 var updateView = async (button) => {
-    var base = 'http://localhost:5000'
 
-    var queryCounter = 0
     
     var filter_code = document.querySelector("#code-filter").value
     var filter_title = document.querySelector("#title-filter").value
@@ -18,7 +16,7 @@ var updateView = async (button) => {
         }
        
         let param = filter_code
-        api = `${base}/api/by_course_code/${param}`
+        api = `api/by_course_code/${param}`
     } 
     else if (button.dataset.filter == "by_title") {
         if (filter_title.length == 0) {
@@ -26,12 +24,12 @@ var updateView = async (button) => {
         }
 
         let param = filter_title
-        api = `${base}/api/by_title/${param}`
+        api = `api/by_title/${param}`
     }
     else if (button.dataset.filter == "by_level") {
 
         let param = filter_level
-        api = `${base}/api/by_level/${param}`
+        api = `api/by_level/${param}`
     }
     else if (button.dataset.filter == "by_instructor") {
         if(filter_name.length == 0) {
@@ -39,7 +37,7 @@ var updateView = async (button) => {
         }
 
         let param = filter_name
-        api = `${base}/api/by_instructor/${param}`
+        api = `api/by_instructor/${param}`
     }
     else if (button.dataset.filter == "combined_query") { 
         if(((filter_combined_level.length > 0) && (filter_combined_name.length == 0)) || ((filter_combined_name.length > 0) && (filter_combined_level.length == 0)) ) {
@@ -48,7 +46,7 @@ var updateView = async (button) => {
 
         let param1 = filter_combined_name
         let param2 = filter_combined_level
-        api = `${base}/api/combined_query/${param1}/${param2}`
+        api = `api/combined_query/${param1}/${param2}`
     }
 
     const data = await fetch(api)
